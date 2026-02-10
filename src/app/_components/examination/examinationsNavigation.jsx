8b97527ee4356ab2334b752c4ce416ecd5540bf7
etukudoemem <saspiee@gmail.com>
2026-02-10 12:59:17 +0100

    fixed exam pages navigation

--- src/app/_components/examination/examinationsNavigation.jsx
+++ src/app/_components/examination/examinationsNavigation.jsx
@@ -5,7 +5,6 @@ import { usePathname } from "next/navigation"
 
 export const ExaminationsNavigation = () => {
 
-    const baseURL = "http://localhost:3000/examinations"
     const baseStyles = `flex justify-center items-center h-[76px] py-[26px] px-[25px] text-[#858688] border-[#F2F4F7]`
     const activeStyles = `flex justify-center items-center h-[76px] py-[26px] px-[22px] text-[#2E8BC0] border-[#2E8BC0]`
     
@@ -13,12 +12,12 @@ export const ExaminationsNavigation = () => {
     
     return (
         <nav className="flex text-xl">
-            <Link href={`${baseURL}`}
+            <Link href={`/examinations`}
                 className={`w-[212px] border-b-2 cursor-pointer ${pathName === "/examinations" ? `${activeStyles}` : `${baseStyles}`}`}
             >
                 Upcoming Exams
             </Link>
-            <Link href={`${baseURL}/practice-tests`}
+            <Link href={`/examinations/practice-tests`}
                 className={`w-[276px] border-b-2 cursor-pointer ${pathName === "/examinations/practice-tests" ? `${activeStyles}` : `${baseStyles}`}`}
             >
                 Practice Tests
