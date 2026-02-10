10e8d33f3bea1ca9c1e95fa0092a22b1976302f0
etukudoemem <saspiee@gmail.com>
2026-02-10 15:46:46 +0100

    added dashboard timetable

--- src/app/_components/cards/icon.jsx
+++ src/app/_components/cards/icon.jsx
@@ -1,14 +1,14 @@
 import Image from "next/image"
 
 
-export const Icon = ({ iconStyle, ImgSrc }) => {
+export const Icon = ({ iconStyle, ImgSrc, ImgWidth, ImgHeight }) => {
 
     return (
         <Image 
             src={ImgSrc}
             alt="image"
-            width={30}
-            height={24}
+            width={ImgWidth}
+            height={ImgHeight}
             className={`${iconStyle}`}
         />
     )
