9e97e5b15bbc4a670fb30249e8ccbb7a57eb738c
etukudoemem <saspiee@gmail.com>
2026-03-27 20:53:02 +0100

    moved examination pages to the academics folder

---
+++ src/app/schools/secondarySchool/student/academics/examinations/_components/pageSelect.jsx
@@ -0,0 +1,28 @@
+import { Button } from "../../../../../../_components/cards/button";
+
+export const PageSelect = ({ question, currentQuestion, setCurrentQuestion }) => {
+
+    const allQuestions = () => {
+        let list = []
+        for (let index = 1; index <= question?.total; index++) {
+            list.push(index);
+        }
+        return list
+    }
+
+    return (
+        <main className="w-fit flex py-4 px-2 items-center gap-x-[24px]">
+        {
+            allQuestions().map((q) => 
+                <Button 
+                    key={q} 
+                    onClick={() => setCurrentQuestion(q)}
+                    buttonStyle={`w-[50px] h-[50px] flex justify-center items-center text-2xl font-bold hover:scale-105 duration-200 cursor-pointer ${currentQuestion === q ? "text-white bg-[#216388]" : "text-[#216388] bg-[#EAF3F9]"} rounded-full `}
+                >
+                    {q}
+                </Button>
+            )
+        } 
+        </main>
+    )
+}
